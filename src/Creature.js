6f4d80d63cafa0f64cd12d314d5509ddd0c13f6f
Jeff Linse <jeff@jefflinse.com>
2019-03-20 17:49:37 -0700

    Remove manual physical traits and integrate matter-js

--- src/Creature.js
+++ src/Creature.js
@@ -1,23 +1,9 @@
-import PhysicalObject from "./PhysicalObject";
-
-class Creature extends PhysicalObject {
-
-    constructor(location) {
-        super(location);
-    }
+class Creature {
 
     tick() {
         // PhysicalObject tick should always be last
         // as it depends on modifications to acceleration, etc.
         super.tick();
-    }
-
-    draw(graphics) {
-        graphics.drawCircle(this.location, 20, {
-            fillStyle: "#0000FF",
-			globalAlpha: 1,
-			lineWidth: 1
-		});
     }
 }
 
--- src/Universe.js
+++ src/Universe.js
@@ -1,32 +1,43 @@
-import Graphics from "./Graphics";
 import Creature from "./Creature";
-import Vector from "./Vector";
+import Matter from "matter-js";
+
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
 
 class Universe {
 
     constructor() {
-        this.reset();
+        this.physicsEngine = Engine.create();
     }
 
     setup(canvas, numCreatures) {
         this.canvas = canvas;
-        this.graphics = new Graphics(this.canvas);
-        
-        for (let i = 0; i < numCreatures; i++) {
-            let location = new Vector(
-                Math.random() * this.canvas.width,
-                Math.random() * this.canvas.height
-            );
-            this.creatures.alive.push(new Creature(location));
-        }
+
+        // create a renderer
+        this.renderer = Render.create({
+            canvas: canvas,
+            engine: this.physicsEngine,
+        });
+
+        // create two boxes and a ground
+        var boxA = Bodies.rectangle(400, 200, 80, 80);
+        var boxB = Bodies.rectangle(450, 50, 80, 80);
+        var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+
+        // add all of the bodies to the world
+        World.add(this.physicsEngine.world, [boxA, boxB, ground]);
+
+        // run the engine
+        //Engine.run(this.physicsEngine);
+
+        // run the renderer
+        Render.run(this.renderer);
     }
 
     tick() {
-        this.graphics.drawBackground();
-        this.creatures.alive.forEach(creature => {
-            creature.tick();
-            creature.draw(this.graphics);
-        });
+        Engine.update(this.physicsEngine, 1000 / 60);
     }
 
     reset() {
