3c022e587e3e2a4bc0f87eb47f617ea8797bb53a
Jeff Linse <jeff@jefflinse.com>
2019-05-31 14:47:42 -0700

    Updates

---
+++ precache-manifest.ce20200b6b6fc300f0c8fe4d4a717377.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75acd7633c03b0a62c73",
+    "url": "/critters/static/css/main.802f3d66.chunk.css"
+  },
+  {
+    "revision": "75acd7633c03b0a62c73",
+    "url": "/critters/static/js/main.0573ab54.chunk.js"
+  },
+  {
+    "revision": "477f29a4e1a1bf0c19ca",
+    "url": "/critters/static/js/runtime~main.ab4a8da7.js"
+  },
+  {
+    "revision": "02aecee3f250de253a0c",
+    "url": "/critters/static/js/2.826223e0.chunk.js"
+  },
+  {
+    "revision": "840f575ecccbfffb1745f311a66cb94b",
+    "url": "/critters/index.html"
+  }
+];
